6680673f20dc614be09d109a01dd8dd1fb35f9d0
bodaraharshil <developer.harshilbodara@gmail.com>
2021-07-15 17:25:23 +0530

    craeted post api for delete user and created post api for update password

--- server/src/schema/Mutations/User.ts
+++ server/src/schema/Mutations/User.ts
@@ -1,6 +1,7 @@
-import { GraphQLString } from "graphql";
+import { GraphQLID, GraphQLString } from "graphql";
 import { UserType } from "../TypeDefs/User";
 import { Users } from "../../entits/Users";
+import { MessageType } from "../TypeDefs/Message";
 
 export const CREATE_USER = {
   type: UserType,
@@ -15,13 +16,64 @@ export const CREATE_USER = {
       type: GraphQLString,
     },
   },
-  resolve(parent: any, args: any) {
+  async resolve(parent: any, args: any) {
     const { name, username, password } = args;
-    Users.insert({
+    await Users.insert({
       name,
       username,
       password,
     });
     return args;
+  },
+};
+
+export const UPDATE_PASSWORD = {
+  type: MessageType,
+  args: {
+    username: {
+      type: GraphQLString,
+    },
+    oldpassword: {
+      type: GraphQLString,
+    },
+    newpassword: {
+      type: GraphQLString,
+    },
+  },
+  async resolve(parent: any, args: any) {
+    const { name, username, oldpassword, newpassword } = args;
+    const user = await Users.findOne({ username: username });
+    const userPassword = user?.password;
+    if (oldpassword === userPassword) {
+      const data = await Users.update(
+        { username: username },
+        { password: newpassword }
+      );
+      if (data) {
+        return {
+          success: true,
+          message: "Password updated successfully",
+        };
+      }
+    } else {
+      throw new Error("Password does't match");
+    }
+  },
+};
+
+export const DELETE_USER = {
+  type: MessageType,
+  args: {
+    id: {
+      type: GraphQLID,
+    },
+  },
+  async resolve(parent: any, args: any) {
+    const id = args.id;
+    await Users.delete(id);
+    return {
+      success: true,
+      message: "User deleted successfully",
+    };
   },
 };
